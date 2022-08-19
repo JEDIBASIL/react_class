0cb64f5645a636e44a135800bad2bffe09540628
Jedi <jedidiahbasil@gmail.com>
2022-08-19 17:04:54 +0100

    fixed errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,19 @@ const Navbar = () => {
   return (
     <div className="navbar">
         <div className="logo">
-            <a href="#">logo</a>
+            <a href="/">logo</a>
         </div>
 
         <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Pricing</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact us</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">Pricing</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Contact us</a></li>
         </ul>
 
         <div className='navBtn'>
-        <a href="#"><button className='signInBtn'>Sign in</button></a>
-            <a href="#"><button className='signUpBtn'>Sign up</button></a>
+        <a href="/"><button className='signInBtn'>Sign in</button></a>
+            <a href="/"><button className='signUpBtn'>Sign up</button></a>
         </div>
     </div>
   )
